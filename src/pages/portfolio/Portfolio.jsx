b1f9dab2e0768affbbc839561f291281427e697b
Prusoth <mrprusothaman@gmail.com>
2025-01-31 05:29:33 +0530

    Web error 2

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { portfolio} from '../../data';
 import PortfolioItem from '../../components/PortfolioItem';
-import "./portfolio.Css"
+import "./portfolio.css"
 
 const Portfolio = () => {
   return (
